4f0fdd73987e4c6e29485b5f9e63d5eb4cc08b08
YeYerin <parkyerin30@gmail.com>
2019-01-17 19:43:35 +0900

    init

---
+++ 3차시.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PARK YERIN</title>
+        <link href='./3차시.css' rel='stylesheet'>
+        <link href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+        <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
+    </head>
+    <body>
+        <div class="back"></div>
+        <div class="icon"><img src="./arrow.png" style="width:80px;"></div>
+        <div class="main">
+            <div class="name">
+                <div class="yerin">Park Ye-rin</div>
+                <div class="age">Eighteen Web Front Develop & Design</div>
+            </div>
+            <div class="school">
+                <div class="dsm">Daedeok Software Meister High School</div>
+                <div class="class">Software Develop 2 - 1<br>Stupid Seungwoo</div>
+            </div>
+        </div>
+        <div class="two">
+                <div class="club">CLUB</div>
+        </div>
+        <div class="three">
+            <div class="booktopia">BOOKTOPIA</div>
+            <div class="book">대마고 도서부 북토피아에서 도서부원으로 활동하며<br>문학 소녀로 활동하고 있습니다.</div>
+        </div>
+        <div class="four">
+            <div class="dormitory">DSM DORMITORY</div>
+            <div class="dor">기숙사 생활을 하는 대마고 학생들을 위한 기숙사 학생 자치회에서<br>학생들의 기숙사 생활을 더욱 즐겁고 편리하게 하기 위해 노력하고 있습니다.</div>
+        </div>
+        <div class="five">
+            <div class="afron">AFRON</div>
+            <div class="af">보안 스터디와 웹 개발을 하는 전공동아리로,<br>리버싱을 공부하고 공동구매사이트, "09"를 개발했습니다.</div>
+        </div>
+    </body>
+</html>
